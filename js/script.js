538c61dc5db93d43a99d4f74b6aebd5fd27fd054
Renuka Prasad <renu.yarasu@gmail.com>
2021-08-05 23:08:39 +0530

    JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // Create a Website with Video Background
+
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+})
